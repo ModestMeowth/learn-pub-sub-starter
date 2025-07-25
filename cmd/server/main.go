085d0385d9a7a1ee1fb7b482f314d693624c0096
Daniel Eberle <daniel.j.eberle@protonmail.com>
2025-07-25 15:20:20 -0500

    added pubsub internal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/ModestMeowth/learn-pub-sub-starter/internal/pubsub"
+	"github.com/ModestMeowth/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const serverUrl = "amqp://guest:guest@localhost:5672"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+    conn, err := amqp.Dial(serverUrl)
+    if err != nil {
+        log.Fatalf("error: %v\n", err.Error())
+    }
+    defer conn.Close()
+
+    fmt.Printf("Connection to %s successful\n", serverUrl)
+
+    signalChan := make(chan os.Signal, 1)
+    signal.Notify(signalChan, os.Interrupt)
+    <-signalChan
+
+    c, err := conn.Channel()
+    if err != nil {
+        log.Fatalf("error: %v\n", err.Error())
+    }
+    defer c.Close()
+
+    err = PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+        IsPaused: true,
+    })
+
+    fmt.Printf("\nInterrupt signal received, shutting down\n")
 }
